04b6f0180cd64959c73b39ab6185c17ec747ead4
Bikash Maity <130330676+PROFESSOR-TG@users.noreply.github.com>
2023-04-10 09:06:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ logging.getLogger("cinemagoer").setLevel(logging.ERROR)
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
-from database.users_chats_db import db
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
-from utils import temp
+from FilmCorner_Robot import API_ID, API_HASH, BOT_TOKEN, temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 
@@ -20,7 +18,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            name=SESSION,
+            "FilmCorner_Robot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
@@ -30,17 +28,15 @@ class Bot(Client):
         )
 
     async def start(self):
-        b_users, b_chats = await db.get_banned()
-        temp.BANNED_USERS = b_users
-        temp.BANNED_CHATS = b_chats
         await super().start()
         await Media.ensure_indexes()
         me = await self.get_me()
+        temp.PYRO_VERSION = __version__
         temp.ME = me.id
-        temp.U_NAME = me.username
-        temp.B_NAME = me.first_name
+        temp.Bot_Username = me.username
+        temp.Bot_Name = me.first_name
         self.username = '@' + me.username
-        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")        
+        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) Started on @{me.username}")                
 
     async def stop(self, *args):
         await super().stop()
