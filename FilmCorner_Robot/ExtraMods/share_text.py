df0c5e3c1b0342d0ddab8eb892610aa94fef7b3b
Bikash Maity <130330676+PROFESSOR-TG@users.noreply.github.com>
2023-04-12 20:33:18 +0530

    Update share_text.py

--- FilmCorner_Robot/ExtraMods/share_text.py
+++ FilmCorner_Robot/ExtraMods/share_text.py
@@ -1,16 +1,15 @@
 import os
 from pyrogram import Client, filters
 from urllib.parse import quote
-from info import SUPPORT_CHAT
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def share_link(text: str) -> str:
     return "**Here is Your Sharing Text 👇**\n\nhttps://t.me/share/url?url=" + quote(text)
 
-@Client.on_message(filters.command(["share", "sharetext", "st", "stxt", "shtxt", "shtext"]))
+@Client.on_message(filters.command(["share_text", "share", "sharetext",]))
 async def share_text(client, message):
     reply = message.reply_to_message
-    reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
+    reply_id = message.reply_to_message.id if message.reply_to_message else message.id
     input_split = message.text.split(None, 1)
     if len(input_split) == 2:
         input_text = input_split[1]
@@ -18,17 +17,13 @@ async def share_text(client, message):
         input_text = reply.text or reply.caption
     else:
         await message.reply_text(
-            text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            "Support Chat", url=f"https://t.me/{SUPPORT_CHAT}")
-                    ]                
-                ]
-            ),
-            reply_to_message_id=reply_id
-        )
+            text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",                
+            reply_to_message_id=reply_id,               
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Support Chat", url=f"https://t.me/MKN_BOTZ_DISCUSSION_GROUP")]])
+            )                                                   
         return
-    await message.reply_text(share_link(input_text), reply_to_message_id=reply_id)
-        
+    await message.reply_text(
+        text=f"**Here is Your Sharing Text 👇**\n\nhttps://t.me/share/url?url=" + quote(input_text),
+        reply_to_message_id=reply_id,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("♂️ Share", url=f"https://t.me/share/url?url={quote(input_text)}")]])       
+    )
