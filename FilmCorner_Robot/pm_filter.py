e768e6cbdf7d76aa36c90f437fe5e6520587d637
Bikash Maity <130330676+PROFESSOR-TG@users.noreply.github.com>
2023-04-13 08:41:18 +0530

    Update pm_filter.py

--- FilmCorner_Robot/pm_filter.py
+++ FilmCorner_Robot/pm_filter.py
@@ -28,16 +28,7 @@ async def give_filter(client, message):
     if k == False:
         await auto_filter(client, message)
 
-@Client.on_callback_query(filters.regex(r"^close_data"))
-async def close_data(bot, query):
-    ident, req, key, offset = query.data.split("_")
-    if int(req) not in [query.from_user.id, 0]:
-        return await query.answer(script.ALRT_TXT.format(query.from_user.first_name), show_alert=True)
-    try:
-        query.data = int(close_data)
-    except:
-        query.data = 0
-           
+          
 @Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
